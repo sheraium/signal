95ecb638264dc559c585600fbd37226f4d0c834a
Peter <sheraium@gmail.com>
2020-04-04 01:26:37 +0800

    add Words.cs/WordsTests.cs

---
+++ src/MPLC.UnitTests/WordsTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MPLC.UnitTests
+{
+    [TestFixture]
+    public class WordsTests
+    {
+        private const string StartAddress = "D100";
+        private IMPLCProvider _mplc;
+        private Words _words;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mplc = Substitute.For<IMPLCProvider>();
+            _words = new Words(_mplc, StartAddress, 2);
+        }
+
+        [Test]
+        public void set_data()
+        {
+            _words.SetData(new[] { 1, 2 });
+
+            _mplc.Received().WriteWords(StartAddress, Arg.Is<int[]>(x =>
+                x[0] == 1 &&
+                x[1] == 2));
+        }
+
+        [Test]
+        public void get_data()
+        {
+            _mplc.ReadWords(StartAddress, 2).Returns(new[] { 1, 2 });
+
+            var actual = _words.GetData();
+
+            actual.Should().BeEquivalentTo(new[] { 1, 2 });
+        }
+    }
+}
---
+++ src/MPLC/Words.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace MPLC
+{
+    public class Words
+    {
+        private readonly int _length;
+        private readonly IMPLCProvider _mplc;
+        private readonly string _startAddress;
+
+        public Words(IMPLCProvider mplc, string startAddress, int length)
+        {
+            _mplc = mplc;
+            _startAddress = startAddress;
+            _length = length >= 1 ? length : 1;
+        }
+
+        public int[] GetData()
+        {
+            return _mplc.ReadWords(_startAddress, _length);
+        }
+
+        public async Task<int[]> GetDataAsync()
+        {
+            return await _mplc.ReadWordsAsync(_startAddress, _length);
+        }
+
+        public void SetData(int[] data)
+        {
+            _mplc.WriteWords(_startAddress, data.Take(_length).ToArray());
+        }
+
+        public async Task SetDataAsync(int[] data)
+        {
+            await _mplc.WriteWordsAsync(_startAddress, data.Take(_length).ToArray());
+        }
+    }
+}
